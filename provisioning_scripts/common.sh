cac5b875f613005e6cde5be00e8a4bec12397e47
Ady Romantika <ady.romantika@gmail.com>
2021-08-10 01:19:35 +0800

    Tidy up

--- provisioning_scripts/common.sh
+++ provisioning_scripts/common.sh
@@ -2,31 +2,26 @@
 
 # Add kubernetes repository to sources.list
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
-echo deb http://apt.kubernetes.io/ kubernetes-xenial main > /etc/apt/sources.list.d/kubernetes.list
+echo deb http://apt.kubernetes.io/ kubernetes-xenial main | tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
+
+# Add the Docker apt repository
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install packages
 apt-get update
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg2 \
+apt-get install -y apt-transport-https ca-certificates software-properties-common gnupg2 \
   kubelet=1.22.0-00 \
   kubeadm=1.22.0-00 \
   kubectl=1.22.0-00 \
-  kubernetes-cni=0.8.7-00
-
-# Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-# Add the Docker apt repository
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Install Docker CE
-apt-get update && apt-get install -y \
+  kubernetes-cni=0.8.7-00 \
   containerd.io=1.4.9-1 \
   docker-ce=5:20.10.8~3-0~ubuntu-$(lsb_release -cs) \
   docker-ce-cli=5:20.10.8~3-0~ubuntu-$(lsb_release -cs)
 
-# Set up the Docker daemon
+# Set up the Docker daemon https://kubernetes.io/docs/setup/production-environment/container-runtimes/#docker
 cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
